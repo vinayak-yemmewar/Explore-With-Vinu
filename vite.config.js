aa474971a07aef7b53a15f3d04041d5ed2d2a0a8
Vinayak Yemmewar <vgyemmewar@gmail.com>
2024-09-25 11:49:31 +0530

    intital commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Explore-With-Vinu/",
+  base:"/Explore-With-Vinu/", 
   plugins: [react()],
 })
